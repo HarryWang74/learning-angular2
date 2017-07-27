aa8187ddfc07960c815f2032ddf5e019d2984c86
Harry <harry74323@gmail.com>
2017-07-27 17:24:57 +1000

    define product interface

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../i-product';
 
 @Component({
   selector: 'app-product-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  products: any = 
+  products: IProduct[] = 
   [
     {
       "productId": 1,
